8ae3360b2a658e93c969d6aff233d260f38a2765
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-3.local>
2024-12-14 11:17:34 +0530

    init

--- 01postres.sql
+++ 01postres.sql
@@ -689,3 +689,9 @@ SELECT manufacturer, SUM(price * units_sold)
 FROM phones 
 GROUP BY manufacturer
 HAVING SUM(price * units_sold) > 2000000;
+
+
+--sorting
+SELECT * FROM products ORDER BY price;
+SELECT * FROM products ORDER BY price ASC;
+SELECT * FROM products ORDER BY price DESC;
