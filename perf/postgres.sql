3d29ef9d82fedd9e47d5f6f9625c4d9d7789f01a
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-3.local>
2024-12-31 16:58:33 +0530

    init

--- perf/postgres.sql
+++ perf/postgres.sql
@@ -5,3 +5,5 @@ SHOW data_directory;
 
 -- This will then list out the name of all the different databases inside of our Postgres installation and an internal identifier that is being used for each one.
 SELECT oid, datname FROM pg_database;
+
+SELECT * FROM pg_class;
