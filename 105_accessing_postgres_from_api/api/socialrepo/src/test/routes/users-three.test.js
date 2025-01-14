c0ebeac522baacc1778cca015e4f272bab997aff
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-3.local>
2025-01-14 16:20:04 +0530

    init

--- 105_accessing_postgres_from_api/api/socialrepo/src/test/routes/users-three.test.js
+++ 105_accessing_postgres_from_api/api/socialrepo/src/test/routes/users-three.test.js
@@ -1,32 +1,29 @@
-const request = require('supertest');
-const buildApp = require('../../app');
-const UserRepo = require('../../repos/user-repo');
-const pool = require('../../pool');
+const request = require("supertest");
+const buildApp = require("../../app");
+const UserRepo = require("../../repos/user-repo");
+const Context = require("../context");
+
+let context;
 
 //A before all statement is going to take a function, That function will then be executed and we're going to wait for that function to be executed before we run any other tests inside this file.
-beforeAll(() => {
-    return pool.connect({
-        host: 'localhost',
-        port: 5432,
-        database: 'socialnetwork-test',
-        user: 'anishkumardubey',
-        password: '',
-    });
+beforeAll(async () => {
+  context = await Context.build();
 });
 
 afterAll(() => {
-    return pool.close();
-})
+  return context.close();
+});
 
-it('create a user', async function() {
+it("create a user", async function () {
+  const startingCount = await UserRepo.count();
+  //expect(startingCount).toEqual(0);
 
-    const startingCount = await UserRepo.count();
-    //expect(startingCount).toEqual(0);
+  await request(buildApp())
+    .post("/users")
+    .send({ username: "test user", bio: "test bio" })
+    .expect(200);
 
-    await request(buildApp()).post('/users').send({username:'test user', bio: 'test bio'}).expect(200);
-
-    const finishingCount = await UserRepo.count();
-    // expect(finishingCount).toEqual(1);
-    expect(finishingCount - startingCount).toEqual(1);
-
-})
+  const finishingCount = await UserRepo.count();
+  // expect(finishingCount).toEqual(1);
+  expect(finishingCount - startingCount).toEqual(1);
+});
