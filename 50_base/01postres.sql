9a85de36b6e84f0690eb3ca1ef6c4d5f34c88d39
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-3.local>
2025-01-15 07:29:07 +0530

    init

--- 50_base/01postres.sql
+++ 50_base/01postres.sql
@@ -1,39 +1,5 @@
 
 
--- SERIAL tells postgres to generate values for this column automatically
-CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-  username VARCHAR(50)
-);
-
-INSERT INTO users (username)
-VALUES ('anish'),('manish'),('suresh'),('mahesh');
-
-INSERT INTO 
-    users (username)
-VALUES
-    ('anish'),('manish'),('suresh'),('mahesh');
-
---foreign key
-CREATE TABLE photos (
-	id SERIAL PRIMARY KEY,
-  url VARCHAR(200),
-  user_id INTEGER REFERENCES users(id)
-);
-
-INSERT INTO photos (url, user_id)
-VALUES ('http://one.jpg', 4);
-
-INSERT INTO
-    photos (url, user_id)
-VALUES
-    ('http://one.jpg', 1),
-    ('http://343.jpg', 1),
-    ('http://565.jpg', 1),
-    ('http://3453.jpg', 2),
-    ('http://dfd.jpg', 3),
-    ('http://sqweq.jpg', 4);
-
 -- joining 2 tables
 SELECT url, username FROM photos
 JOIN users ON users.id = photos.user_id;
